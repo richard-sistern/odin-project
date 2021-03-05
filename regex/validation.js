cdde28c8ded5bfbe985f2c218fe2f6115d19f365
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-05 14:30:34 +0000

    add regex validation for username, password, slug and email

--- regex/validation.js
+++ regex/validation.js
@@ -2,7 +2,11 @@
 const inputs = document.querySelectorAll('input');
 
 const patterns = {
-    telephone:/^\d{11}$/
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 // validation function
