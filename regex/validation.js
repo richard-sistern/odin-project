c4c64592b2abdc3a61f2bdd88b104d908efac23d
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-05 13:57:59 +0000

    add regex validation for telephone

--- regex/validation.js
+++ regex/validation.js
@@ -1,1 +1,23 @@
 // validation script here
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone:/^\d{11}$/
+};
+
+// validation function
+function validate(field, regex) {
+    //console.log(regex.test(field.value));
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        //console.log(e.target.attributes.name.value)
+        validate(e.target, patterns[e.target.attributes.name.value])
+    });
+});
